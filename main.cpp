8bc4a8fe1b64fed0519094135e75437664db49d5
Santiago Ferreyra <sferreyra0@ivc.edu>
2022-10-17 17:20:04 -0700

    Push button closes window

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "mainwindow.h"
+#include <QDebug>
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    qDebug() << "Hello worlds";
+    return a.exec();
+
+}
